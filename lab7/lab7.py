599281029f9ff523c6d0c72683fe70024f289255
Gosia <malgorzata.czarne@gmail.com>
2022-04-30 00:25:57 +0200

    uczenie nienadzorowane

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 from sklearn.datasets import fetch_openml
@@ -12,14 +12,14 @@ X = mnist["data"]
 y = mnist["target"]
 
 
-# In[2]:
+# In[3]:
 
 
 from sklearn.cluster import KMeans 
 from sklearn.metrics import silhouette_score
 
 
-# In[3]:
+# In[4]:
 
 
 lista =[]
@@ -30,7 +30,7 @@ for k in range(8,13):
     print(silhouette_score(X, kmeans.labels_))
 
 
-# In[4]:
+# In[5]:
 
 
 import pickle
@@ -38,7 +38,7 @@ file = open("kmeans_sil.pkl","wb")
 pickle.dump(lista, file)
 
 
-# In[5]:
+# In[6]:
 
 
 file = open("kmeans_sil.pkl","rb")
@@ -47,20 +47,20 @@ file.close()
 print(a)
 
 
-# In[6]:
+# In[7]:
 
 
 kmeans = KMeans(n_clusters=10, random_state=42)
 y_pred = kmeans.fit_predict(X)
 
 
-# In[7]:
+# In[8]:
 
 
 kmeans.labels_
 
 
-# In[8]:
+# In[9]:
 
 
 from sklearn.metrics import confusion_matrix
@@ -68,37 +68,37 @@ cf = confusion_matrix(y, y_pred)
 print(cf)
 
 
-# In[9]:
+# In[10]:
 
 
 print(y_pred)
 print(y)
 
 
-# In[10]:
+# In[11]:
 
 
 lista2 = []
 for i in cf:
-    lista2.append(i[i.argmax()])
+    lista2.append(i.argmax())
 lista2 = list(set(lista2))
-lista2.sort(reverse=True)
+lista2.sort()
 
 
-# In[11]:
+# In[12]:
 
 
 print(lista2)
 
 
-# In[12]:
+# In[13]:
 
 
 file = open("kmeans_argmax.pkl","wb")
 pickle.dump(lista2, file)
 
 
-# In[13]:
+# In[14]:
 
 
 file = open("kmeans_argmax.pkl","rb")
@@ -107,34 +107,33 @@ file.close()
 print(a)
 
 
-# In[14]:
+# In[15]:
 
 
 from sklearn.cluster import DBSCAN
 
 
-# In[15]:
+# In[46]:
 
 
 lista3 = []
-lista4 =[]
+lista4 = []
 for i in range(0, 300):
-    for j in range(0, 300):
-        if i != j:
-            lista3.append(np.linalg.norm(X[i]-X[j]))
+    for j in range(i+1, 300):
+        lista3.append(np.linalg.norm(X[i]-X[j]))
 lista3.sort()
 lista4 = lista3[:10]
-lista4.sort(reverse = True)
+lista4.sort()
 
 
-# In[16]:
+# In[37]:
 
 
 file = open("dist.pkl","wb")
 pickle.dump(lista4, file)
 
 
-# In[17]:
+# In[38]:
 
 
 file = open("dist.pkl","rb")
@@ -143,15 +142,14 @@ file.close()
 print(a)
 
 
-# In[18]:
+# In[40]:
 
 
-lista4.sort()
 s = (lista4[0] + lista4[1] + lista4[2]) / 3.0
 print(s)
 
 
-# In[19]:
+# In[41]:
 
 
 lista5 = []
@@ -163,20 +161,20 @@ while i <=  s + 0.1 * s:
     i += i * 0.04
 
 
-# In[20]:
+# In[42]:
 
 
 print(lista5)
 
 
-# In[21]:
+# In[43]:
 
 
 file = open("dbscan_len.pkl","wb")
 pickle.dump(lista5, file)
 
 
-# In[22]:
+# In[44]:
 
 
 file = open("dbscan_len.pkl","rb")
